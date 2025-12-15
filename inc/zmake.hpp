8d116e5b523598dc3d02594cad90af91a2dcde90
ProsperousTroll <ivekilled4dogs@gmail.com>
2025-12-14 17:11:08 -0800

    feat: core command map added. TODO: fix input parsing

--- inc/zmake.hpp
+++ inc/zmake.hpp
@@ -1,6 +1,12 @@
+#pragma once
+#include <functional>
+#include <unordered_map>
 #include <vector>
 #include <iostream>
 #include <string>
+#include "parser.hpp"
+#include <cstring>
+#include <iostream>
 
 namespace ZMake {
    inline std::string version{"0.0.1"};
@@ -21,49 +27,51 @@ namespace ZMake {
       "--version, -v : Show the installed ZMake version. \n",
       "--parse, -p : Run a parser test on the project file. \n",
    };
-
    inline void printUsage(){
       for (auto& line : usage){
          std::cout << line;
       }
    }
 
-   struct UserInput {
-      std::string type;
-      std::string value;
+   // world's babiest error struct aww :pleading:
+   struct Error {
+      std::string error;
 
-      UserInput(std::string const& t, std::string const& v){
-         type = t;
-         value = v;
+      std::string what(){
+         return {"ERROR: " + error};
+      }
+
+      Error(std::string const& in){
+         error = in;
       }
    };
 
-   inline void runCommands(UserInput const& in){
-      if(in.value == "new"){
-         std::cout << "Running NEW\n";
-      }
-      if(in.value == "--help"){
-         printUsage();
-      }
-   }
+   using Args = std::vector<std::string>;
+   using Handler = std::function<void(Args const& arg)>;
 
-   inline void runFlags(UserInput const& in){
-      if(in.value == "--help"){
-         printUsage();
-      }
-   }
+   inline std::unordered_map<std::string, Handler> CommandHandler{
+      {"--version", [](Args const& arg){ std::cout << "ZMake " << version << '\n'; }},
+      {"--help", [](Args const& arg){ printUsage(); }},
+      {"--parse", [](Args const& arg){ Parser::parseInput(Parser::projectFile); }},
+      {"new", [](Args const&arg){
+         try {
+            if(arg.size() < 2){
+               throw Error("Please enter project name!");
+            } else if (arg.size() > 2){
+               throw Error("Too many arguments! Please enter a project name only.");
+            }
 
-   enum Commands {
-      NEW,
-      INIT,
-      BUILD,
-      RUN,
-      CLEAN,
-   };
+            std::string projectName{arg[1]};
+            std::cout << "Creating new project directory named '" + projectName + "'...\n";
+         }
 
-   enum Flags {
-      VERSION,
-      HELP,
-      PARSE,
+         catch(Error& e){
+            std::cerr << e.what() << '\n';
+         }
+
+         catch(...){
+            std::cerr << "ERROR: Unknown crash.\n";
+         }
+      }},
    };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
-#include <cstring>
-#include <iostream>
-#include "../inc/parser.hpp"
 #include "../inc/zmake.hpp"
 
-// denable debugging
-#define DEBUG
+// enable debugging
+// #define DEBUG
 
 int main(int argc, char* argv[]){
    if (argc < 2){
@@ -12,18 +9,41 @@ int main(int argc, char* argv[]){
       return 0;
    }
 
-   std::vector<ZMake::UserInput> usrIn;
+   ZMake::Args userIn;
    for(int i{1}; i != argc; ++i){
-      if(!strncmp(argv[i], "-", 1)){
-         usrIn.emplace_back("flag", argv[i]);
-      } else usrIn.emplace_back("cmd", argv[i]);
+      // TODO: fix this.
+      ZMake::Args flag;
+      ZMake::Args cmd;
+      (!strncmp(argv[i], "--", 1)) ? flag.emplace_back(argv[i]) : cmd.emplace_back(argv[i]);
+      for (auto& arg : flag){ userIn.emplace_back(arg); }
+      for (auto& arg : cmd){ userIn.emplace_back(arg); }
    }
 
-   for(auto& arg : usrIn){
-      // DEBUG: Print all arguments and types
-      #ifdef DEBUG
-      std::cout << "Type: " << arg.type << " Value: " << arg.value << '\n';
-      #endif
+   for (auto& arg : userIn){
+      try {
+         if(ZMake::CommandHandler.find(arg) == 0){
+            throw ZMake::Error("argument '" + arg + "' does not exist.");
+         } else ZMake::CommandHandler[arg](userIn);
+      }
+
+      catch(ZMake::Error& e) {
+         std::cerr << e.what() << '\n';
+         return 1;
+      }
+
+      catch(...){
+         std::cerr << "ERROR: Something went HORRIBLY wrong. IDEK what, man. That's crazy what you just did.\n";
+         return 2;
+      }
    }
+
+
+   #ifdef DEBUG
+
+   for (auto& arg : userIn){
+      std::cout << arg << '\n';
+   }
+
+   #endif
    return 0;
 }
